9169b41d44f1e61610c8b7002a712baea8a0e116
olawale-o <omoogunolawale@gmail.com>
2021-08-07 15:18:35 +0100

    Update webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
     ],
   },
   output: {
