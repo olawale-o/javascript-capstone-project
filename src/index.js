b5e31cda9806273f01b6a5d95cc6d2ae2c312967
omoogun olawale <omoogunolawale@gmail.com>
2021-08-07 14:01:16 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './stylesheets/style.css';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const root = document.querySelector('#root');
+  root.textContent = 'Webpack Starter';
+});
