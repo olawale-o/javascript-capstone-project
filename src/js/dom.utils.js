91c3adb8156a812a70bc559abb28a598f4e2699c
olawale-o <omoogunolawale@gmail.com>
2021-08-10 14:34:11 +0100

    Create createReservation in dom.utils.js file

--- src/js/dom.utils.js
+++ src/js/dom.utils.js
@@ -15,3 +15,11 @@ export const createComment = (comment) => {
   ${capitalizeStr(comment.username)}:</span> <span>${comment.comment}</span>`;
   return li;
 };
+
+export const createReservation = (reservation) => {
+  const li = createElement('li', { class: 'comment' });
+  li.innerHTML = `<span>${parseDate(lastReservation.date_start)} -
+  ${parseDate(lastReservation.date_end)}  by  </span> <span>${lastReservation.username}</span>`;
+  return li;
+};
+
